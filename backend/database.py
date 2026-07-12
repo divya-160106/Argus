98f209ba43a4ce85f0405657678e5ad121d88e7c
Divyasree Manikandan <divyasreem1601@gmail.com>
2026-07-12 20:12:07 +0530

    No passsword prints

--- backend/database.py
+++ backend/database.py
@@ -4,11 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.getenv("MONGODB_URI"))
-print(os.getenv("DATABASE_NAME"))
-print(os.getenv("COLLECTION_NAME"))
-
-
 MONGO_URI = os.environ["MONGODB_URI"]
 DATABASE_NAME = os.environ["DATABASE_NAME"]
 COLLECTION_NAME = os.environ["COLLECTION_NAME"]
